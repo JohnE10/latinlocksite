b741ddc4a64ffdcce27ebbd4fe323f4fe7272175
JohnE10 <csworg@gmail.com>
2026-03-03 10:56:54 -0500

    modified next.config.mjs redirects to work with the new subfolder structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,35 +10,35 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/blog',
+        destination: '/latinlock/blog',
         permanent: true,
         basePath: false,
         has: [{ type: 'host', value: 'latinlockblog.stackorbithq.com' }],
       },
       {
         source: '/ecommerce-shipping-trends-2025',
-        destination: '/blog/ecommerce-shipping-trends-2025',
+        destination: '/latinlock/blog/ecommerce-shipping-trends-2025',
         permanent: true,
         basePath: false,
         has: [{ type: 'host', value: 'latinlockblog.stackorbithq.com' }],
       },
       {
         source: '/checkout-mistakes-cost-shopify-sales',
-        destination: '/blog/checkout-mistakes-cost-shopify-sales',
+        destination: '/latinlock/blog/checkout-mistakes-cost-shopify-sales',
         permanent: true,
         basePath: false,
         has: [{ type: 'host', value: 'latinlockblog.stackorbithq.com' }],
       },
       {
         source: '/non-latin-addresses-carrier-issues',
-        destination: '/blog/non-latin-addresses-carrier-issues',
+        destination: '/latinlock/blog/non-latin-addresses-carrier-issues',
         permanent: true,
         basePath: false,
         has: [{ type: 'host', value: 'latinlockblog.stackorbithq.com' }],
       },
       {
         source: '/prevent-non-latin-characters-in-shopify-shipping-address',
-        destination: '/blog/prevent-non-latin-characters-in-shopify-shipping-address',
+        destination: '/latinlock/blog/prevent-non-latin-characters-in-shopify-shipping-address',
         permanent: true,
         basePath: false,
         has: [{ type: 'host', value: 'latinlockblog.stackorbithq.com' }],
